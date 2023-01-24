ef5983687c95a6a1f7bea0fc798bea2ccdae366b
immortalprogrammist <immortalvelial@icloud.com>
2023-01-24 19:40:33 +0100

    set up functionality - Open andClose Modal buttons

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 "use strict";
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+
+console.log(btnsOpenModal);
+
+for (let index = 0; index < btnsOpenModal.length; index++) {
+  btnsOpenModal[index].addEventListener("click", function () {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+  });
+}
+
+btnCloseModal.addEventListener("click", function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+});
